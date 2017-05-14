346d7dcd834cdc6fcd575e0ebc705aae09a655c8
Bujdosó Valentin <valentinbujdoso@gmail.com>
2017-05-14 20:27:16 +0200

    Minor fix

--- php/send_form_email.php
+++ php/send_form_email.php
@@ -17,15 +17,15 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly.');
-		window.location = 'contact.html';
+		alert('Köszönjük az üzenetet. Hamarosan válaszolunk!');
+		window.location = '../contact.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to gordon@template-help.com');
-		window.location = 'contact.html';
+		alert('Hiba történt!');
+		window.location = '../contact.html';
 	</script>
 <?php
 }
